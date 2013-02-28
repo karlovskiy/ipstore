88d36c24839aa2fc8d77e78599e8dc3b71e9dcbe
ravenstar <yexella@gmail.com>
2013-03-01 00:07:35 +0400

    2.1.1-SNAPSHOT version is here!... Equipment view page refactoring

--- src/main/webapp/js/view.js
+++ src/main/webapp/js/view.js
@@ -2,18 +2,17 @@
  * @author karlovsky
  */
 $(document).ready(function () {
-    var del = $("#equipment_delete");
+    var del = $("#delete_btn");
     if (del.length != 0) {
         del.click(function (e) {
             e.preventDefault();
             var location = $(this).attr("href");
-            bootbox.confirm("Do you really want to delete equipment?","No", "Yes", function (yes) {
+            bootbox.confirm("Do you really want to delete ?","No", "Yes", function (yes) {
                 if (yes) {
                     window.location.replace(location);
                 }
             });
         });
     }
-    var copy_to_clipboard = new ZeroClipboard($("#copy_to_clipboard"), { moviePath: "/flash/zeroclipboard.swf"});
 });
 
